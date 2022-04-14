6ff397da324d45d0b2bac2e4a86b7a9792353bbd
Haley Karp <hkarp2000@gmail.com>
2022-04-13 21:43:54 -0400

    Framing the skeleton

--- outfit_finder.py
+++ outfit_finder.py
@@ -5,54 +5,96 @@ class MyCloset:
     
     """
     Function that reads in users closet file
+    Display current closet to the users 
+    (maybe in table format) 
     """
 
-
     """
-    Function that asks user what they want to do
-        pick an outfit 
-        random outfit 
-        add new clothes to closet
-        get rid of clothes from closet
-    
-    """
-    """
-    If pick an outfit for the day: 
+    If user picks an outfit for the day: 
     Function
         An outfit for a casual or formal event?
         What colors are they looking for? 
         What is the weather supposed to be like?
     
-    Ask the user what matters most,
+    """
+    
+    """
+    If user picks an outfit for the day:
+    Function 
+        Ask the user what matters most,
         color/weather or casual/formal? 
         (this will indicate the rating order below) 
-    """
-    
-    """
-    
-    Function 
+        
         Using the users needs:
-            rank the outfit items based 
+            rank the outfit items based on 
             the users priority
-            based on tops/bottoms and shoes
+            for tops/bottoms and shoes
     
     """
     
     """
+    If user picks an outfit for the day:
     Function 
         Using the ranking above,
-         indicate the top items
-         of the tops, shoes and bottoms
+         indicate the #1 tops, shoes and bottoms
          to tell the user the 
          highly rated outfit
     """
     
     """
-    Function 
-        if user picks random outfit, 
-        put together 2 outfits for each 
-        1. outfit for casual vs formal
-        2. outfit for color 
-        3. outfit for weather
+    If user picks based on friends style
+        Ask user to indicate what the friend 
+        is most likely to wear for 
+        tops:
+            short sleeve/long, etc
+        bottoms: knee high, short, long pants, etc
+        shoes: heels, sandals, flip flops 
+    
+    Use the criteria to return an outfit that
+    has all three of those implemented 
+    (need to match certain colors) 
+    
+    """
+    
+    """
+    If user packs for a trip
+    Function
+        Ask user how many days they are
+        staying? 
+        depending on number of days, 
+        give outfits accordingly to wear 
+        each day 
+
+    """
+    
+    """
+    Function
+    If user needs to add clothing items to closet
+    
+    """
+    
+    """
+    Function that saves the created outfits to a new textfile? 
+    Just an idea
+    """
+    
+    """
+    
+    """
+    
+    
+    """
+    I believe this would be the main function:
+    based on the users choice, 
+    certain functions will be called to run 
+    so in this main function, if user picks 
+    the first option, only the functions that 
+    align with that option will be called
+    
+    Function that asks user what they want to do
+        pick an outfit for the day
+        based on friends style 
+        Pack for a trip
+        add new clothes to closet
     
     """
