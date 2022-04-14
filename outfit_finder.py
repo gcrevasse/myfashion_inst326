30a26e705bb87106bec4ed5bac1c079902e74226
Haley Karp <hkarp2000@gmail.com>
2022-04-13 20:40:16 -0400

    Added introduction of possible functions in our class

---
+++ outfit_finder.py
@@ -0,0 +1,58 @@
+class MyCloset:
+    """ Help users pick an outfit based on 
+    criteria given by the user
+    """
+    
+    """
+    Function that reads in users closet file
+    """
+
+
+    """
+    Function that asks user what they want to do
+        pick an outfit 
+        random outfit 
+        add new clothes to closet
+        get rid of clothes from closet
+    
+    """
+    """
+    If pick an outfit for the day: 
+    Function
+        An outfit for a casual or formal event?
+        What colors are they looking for? 
+        What is the weather supposed to be like?
+    
+    Ask the user what matters most,
+        color/weather or casual/formal? 
+        (this will indicate the rating order below) 
+    """
+    
+    """
+    
+    Function 
+        Using the users needs:
+            rank the outfit items based 
+            the users priority
+            based on tops/bottoms and shoes
+    
+    """
+    
+    """
+    Function 
+        Using the ranking above,
+         indicate the top items
+         of the tops, shoes and bottoms
+         to tell the user the 
+         highly rated outfit
+    """
+    
+    """
+    Function 
+        if user picks random outfit, 
+        put together 2 outfits for each 
+        1. outfit for casual vs formal
+        2. outfit for color 
+        3. outfit for weather
+    
+    """
